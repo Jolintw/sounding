25b5f86472eeb8a8c132448a6a1494bb9a4aea6e
Jolin <b07209039@gmail.com>
2024-04-26 21:29:46 +0800

    add pathconfig

---
+++ variable/pathconfig.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+ROOT = Path("D:\NOR10069")
+
+DATA = ROOT / "data"
+STDATA = DATA / "ST"
+ST1 = STDATA / "ST_20240326-0330"
+ST1 = STDATA / "ST_20240330-0404"
+RS41DATA = DATA / "RS41"
+
+PIC  = ROOT / "pic"
+
+
