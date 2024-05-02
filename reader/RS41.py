fix bugs of qv calculation (RH/100)

reader/RS41.py
@@ -1,6 +1,7 @@
 import numpy as np
 from atmospkg.calculation import saturation_mixingratio, wswd_to_uv, potential_temperature
 from datetime import datetime as dd
+from mypkgs.processor.timetools import timestamp_to_datetime
 import os 
 
 class RS41reader:
@@ -22,8 +23,8 @@ class RS41reader:
         for key, value in varnamedict.items():
             vardict[key] = np.loadtxt(filepath, delimiter=',', skiprows=4, unpack=True, usecols=(value))[mask]
         vardict["qvs"] = saturation_mixingratio(vardict["T"], vardict["P"], Tunit="degC", Punit="hPa")
-        vardict["qv"] = vardict["qvs"] * vardict["RH"]
-        vardict["PT"] = potential_temperature(vardict["T"], vardict["P"], Tunit="degC", Punit="hPa")
+        vardict["qv"]  = vardict["qvs"] * vardict["RH"] / 100
+        vardict["PT"]  = potential_temperature(vardict["T"], vardict["P"], Tunit="degC", Punit="hPa")
         vardict["U"], vardict["V"] = wswd_to_uv(vardict["WS"], vardict["WD"], wdunit="deg")
         vardict["timestamp"] = vardict["time"] + self.getzerotimefromfile(filepath)
         
@@ -39,7 +40,7 @@ class RS41reader:
         return filelist
     
     def getfirsttime(self, vardict):
-        return dd.fromtimestamp(vardict["timestamp"][0])
+        return timestamp_to_datetime(vardict["timestamp"][0])
     
     def _create_tempfile_without_nonnumericstr(self, filepath, tempfilename):
         f  = open(filepath)