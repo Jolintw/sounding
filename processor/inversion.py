8dd21ed2fe9596b6fb1b06c5cf6a18d5e9fbdf64
Jolin <b07209039@gmail.com>
2024-05-10 21:38:09 +0800

    add dPTdH_maxind

--- processor/inversion.py
+++ processor/inversion.py
@@ -7,17 +7,16 @@ def find_inversion_layer(PT, H, cloud_mask):
     PT: K
     H: meter
     """
-    inversion_mask = central_diff(PT, H) > 0.005
+    dPTdH = central_diff(PT, H)
+    inversion_mask = dPTdH > 0.005
     inversion_mask = np.logical_and(inversion_mask, ~cloud_mask)
     inversion_layer = Layer()
     inversion_layer.find_layers_from_1Dmask(inversion_mask)
     if inversion_layer.layer_number != 0:
-        inversion_layer.bottom_H  = H[inversion_layer.bottom_ind]
-        inversion_layer.top_H     = H[inversion_layer.top_ind]
-        inversion_layer.bottom_PT = PT[inversion_layer.bottom_ind]
-        inversion_layer.top_PT    = PT[inversion_layer.top_ind]
+        inversion_layer.get_bottom_top_value(H, postfix="H")
+        inversion_layer.get_bottom_top_value(PT, postfix="PT")
+        inversion_layer.get_max_value_in_layer(dPTdH, prefix="dPTdH")
+        inversion_layer.get_max_ind_in_layer(dPTdH, prefix="dPTdH")
         inversion_layer.thickness = inversion_layer.top_H - inversion_layer.bottom_H
-    else:
-        return inversion_layer
     return inversion_layer
 
