64d9f3f444d0a816a4b998347c31440a244b3fed
Jolin <b07209039@gmail.com>
2024-05-16 11:37:36 +0800

    MIDAS_reader (after debug)

--- reader/MIDAS_reader.py
+++ reader/MIDAS_reader.py
@@ -16,7 +16,7 @@ def get_interest_lines(filepath, interest_list):
             continue
         key = line_split[2][1:]
         if key in interest_list:
-            interest_lines[key].append(line.split(",", maxsplit=4)[-1])
+            interest_lines[key].append(line.split(",", maxsplit=3)[-1])
             line_indexs[key].append(i_lines)
     return interest_lines, line_indexs
 
@@ -27,9 +27,12 @@ def readMIDAS(filepath, need_var= None):
     if need_var is None:
         need_var = ["met_bridge", "met_ntu"]
     vars = {}
+    print("readlines...")
     interest_lines, line_indexs = get_interest_lines(filepath, get_interest_list(need_var))
+    print("get time...")
     timestamps = ZDA_to_timestamp(interest_lines[use_time])
     time_list_indexs = time_list_index(line_indexs)
+    print("get data from lines...")
     if "met_bridge" in need_var:
         vars["met_bridge"] = read_met_bridge(interest_lines[met_bridge], time_list_indexs[met_bridge], timestamps)
     if "met_ntu" in need_var:
@@ -42,6 +45,7 @@ def get_interest_list(need_var):
         interest_list += [met_bridge]
     if "met_ntu" in need_var:
         interest_list += [met_ntu]
+    return interest_list
 
 def time_list_index(line_indexs, time_name=use_time):
     """
@@ -57,12 +61,15 @@ def time_list_index(line_indexs, time_name=use_time):
     for key in keys:
         time_list_indexs[key] = []
         i_time_list = 0
+        time_ind_now = time_index[i_time_list]
         for line_ind in line_indexs[key]:
-            time_ind_now = time_index[i_time_list]
-            if line_ind < time_ind_now:
-                time_list_indexs[key].append(i_time_list - 1)
-            else:
+            while line_ind > time_ind_now:
                 i_time_list += 1
+                if i_time_list >= len(time_index):
+                    i_time_list = len(time_index)
+                    break
+                time_ind_now = time_index[i_time_list]
+            time_list_indexs[key].append(i_time_list - 1)
     return time_list_indexs
 
 def ZDA_to_timestamp(time_lines):
@@ -76,18 +83,22 @@ def ZDA_to_timestamp(time_lines):
 
 def read_met_bridge(varlines, index_of_timestamps, timestamps):
     data = {"P":[], "P_time":[], "H":[], "H_time":[], "G":[], "G_time":[]}
+    # print(len(varlines))
     for i_line, line in enumerate(varlines):
         line_split = line.split(",")
         timestamp_ind = index_of_timestamps[i_line]
         varname = line_split[0]
-        data[varname+"_time"].append(timestamps[timestamp_ind])
+        if timestamp_ind < 0:
+            data[varname+"_time"].append(timestamps[0])
+        else:
+            data[varname+"_time"].append(timestamps[timestamp_ind])
         data[varname].append(float(line_split[1]))
     return data
 
 def read_met_ntu(varlines, line_indexs, timestamps, time_name=use_time):
     data = {"RH":[], "T":[], "time":[]}
 
-    zerotime_ntu = line[0].split(",")[1]
+    zerotime_ntu = float(varlines[0].split(",")[1])
     zerotime_ZDA = _get_zerotime_ZDA(line_indexs, timestamps, time_name)
 
     for i_line, line in enumerate(varlines):
